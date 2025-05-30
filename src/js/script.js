219cb7f571cff363ad2fe0b99a317ee4e3118161
Roman <romankurockin473@gmail.com>
2025-05-30 19:39:37 +0300

    beginning work at new section works

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "/src/sass/style.scss";
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+});
+
 
 
