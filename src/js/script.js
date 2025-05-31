4fef426f8ce2632fedf10b9955e61ebbf1a675e7
Roman <romankurockin473@gmail.com>
2025-05-31 14:39:09 +0300

    continue working on the section works

--- src/js/script.js
+++ src/js/script.js
@@ -7,11 +7,43 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import "/src/sass/style.scss";
 
-// init Swiper:
-const swiper = new Swiper('.swiper', {
-    // configure Swiper to use modules
-    modules: [Navigation, Pagination],
-});
+
+try {
+    // init Swiper:
+    new Swiper('.works__slider', {
+
+        slidesPerView: 1,
+        loop: true,
+
+        navigation: {
+            nextEl: ".icon-right-open",
+            prevEl: ".icon-left-open",
+        },
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+
+        breakpoints: {
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5
+            },
+            1920: {
+
+                spaceBetween: 35
+            }
+        },
+        // configure Swiper to use modules
+        modules: [Navigation, Pagination],
+    });
+
+}
+catch (e) { }
+
+
 
 
 
