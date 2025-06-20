5d3c04999410632ac24731db5532cc28be869312
Roman <romankurockin473@gmail.com>
2025-06-21 00:16:14 +0300

    work on project validation has been completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
+import JustValidate from 'just-validate';
 
 
 import 'swiper/css';
@@ -77,7 +78,87 @@ try {
     contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
 } catch (e) { }
 
+try {
+    const validator = new JustValidate('form');
 
+    validator.addField('#name', [
+        {
+            rule: 'required',
+            errorMessage: 'Please fill the name'
+        },
+        {
+            rule: 'minLength',
+            value: 2,
+            errorMessage: 'Min 2 char!'
+        }
+    ])
+        .addField('#email', [
+            {
+                rule: 'required',
+            },
+            {
+                rule: 'email',
+
+            }
+        ])
+        .addField('#question', [
+            {
+                rule: 'required',
+            },
+            {
+                rule: 'minLength',
+                value: 5,
+            }
+        ], {
+            errorsContainer: document
+                .querySelector('#question')
+                .parentElement.querySelector('.error-message')
+        }
+        )
+        .addField('#checkbox', [
+            {
+                rule: 'required',
+            },
+        ], {
+            errorsContainer: document
+                .querySelector('#checkbox')
+                .parentElement.parentElement.querySelector('.checkbox-error-message')
+        })
+} catch (e) {
+
+}
+
+try {
+    const FooterValidator = new JustValidate('.footer__form');
+
+    FooterValidator.addField('#footer__email', [
+        {
+            rule: 'required',
+        },
+        {
+            rule: 'email',
+
+        }
+    ],
+        {
+            errorsContainer: document
+                .querySelector('#footer__email')
+                .parentElement.querySelector('.error-message')
+        }
+    )
+
+        .addField('#footer__checkbox', [
+            {
+                rule: 'required',
+            },
+        ], {
+            errorsContainer: document
+                .querySelector('#footer__checkbox')
+                .parentElement.parentElement.querySelector('.checkbox-error-message')
+        })
+
+} catch (e) {
+}
 
 
 
