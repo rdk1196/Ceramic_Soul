646495f7149448e513e69d4a5c18e58ad7e57f62
Roman <romankurockin473@gmail.com>
2025-06-23 18:00:13 +0300

    finished working on sending forms

--- src/js/script.js
+++ src/js/script.js
@@ -124,6 +124,18 @@ try {
                 .querySelector('#checkbox')
                 .parentElement.parentElement.querySelector('.checkbox-error-message')
         })
+        .onSuccess((event) => {
+            const form = event.currentTarget
+            const formData = new FormData(form);
+
+            fetch("https://httpbin.org/post", {
+                method: "POST",
+                body: formData,
+            }).then(res => res.json()).then(data => {
+                console.log("Success", data);
+                form.reset();
+            });
+        });
 } catch (e) {
 
 }
